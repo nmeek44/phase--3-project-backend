c464fccd5d6a039f2865c77f3b23ebe15c680528
nmeek44 <nmeek44@gmail.com>
2021-06-01 12:38:08 -0400

    racke not working yet

--- app/application.rb
+++ app/application.rb
@@ -4,15 +4,20 @@ class Application
       resp = Rack::Response.new
       req = Rack::Request.new(env)
   
-      if req.path.match(/test/) 
+      if req.path.match(/test/)
         return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
   
+      elsif req.path.match("/photos") && req.get?
+
+        blog_instances_arr = Blog.all
+        [200, {"Content-Type" => "application/json"}, [blog_instances_arr.to_json]]
+      elsif req.path.match("/photos") && req.post?
+        json_data = req.body.read 
+        photo_hash = JSON.parse(json_data)
+        new_data = Blog.create(photo_ids: blog_hash[:photo_ids],user_ids: blog_hash[:user_ids], story: blog_hash[:story])
+        
       else
-        resp.write "Path Not Found"
-  
+        [404, {}, ["Path not found"]]
       end
-  
-      resp.finish
     end
-  
   end
---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+Photo.create([{
+    "photo_url":
+    "category":
+},
+ { 
+    "photo_url":
+    "category":
+}])
+
+Blog.create([{
+    "photo_ids":
+    "user_ids":
+    "story":
+}])
+
+User.create([{
+    "username":
+}])
